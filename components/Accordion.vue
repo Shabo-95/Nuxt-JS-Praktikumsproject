00656978f1ec669ce26d927b58587afcd72df023
shsal <s.saliba@s-v.de>
2021-12-10 16:27:53 +0100

    Accordion Component is now reusable (for Other Content) and inhanced

--- components/Accordion.vue
+++ components/Accordion.vue
@@ -1,78 +1,77 @@
 <template>
-  <!-- First Accordion -->
   <div class="accordion">
-    <AccordionItems v-for="(item, id) in items" :key="id"/>
+    <AccordionItems
+      v-for="(item, id) in items"
+      :key="id"
+      v-bind="item"
+      @clicked="itemClicked"
+      :id="id"
+    />
   </div>
 </template>
 
 <script>
+import AccordionItems from './AccordionItems'
+
 export default {
-  name: "Accordion",
+  name: 'Accordion',
+  components: {
+    AccordionItems,
+  },
   data() {
     return {
-      items: {
-        accordion1: {
-          header: "Begeistert an Zukunftsthemen tüfteln – Selina Stihl",
+      // open: false,
+      // height: 0,
+      items: [
+        {
+          header: 'Begeistert an Zukunftsthemen tüfteln – Selina Stihl',
           body:
-            "<p>Als Mitglied der Familie Stihl bin ich im Beirat tätig, " +
-            "der sich in erster Linie mit der Strategie beschäftigt. Beim STIHL Award war spannend, " +
-            "in welcher Weise die Studierenden unsere strategischen Themenfelder mit verschiedenen " +
-            "aktuellen Entwicklungen und auch ihren Wahrnehmungen und Herangehensweisen in Verbindung " +
-            "gebracht haben. Zwischendurch die Blickrichtung zu ändern und neue Impulse aufzunehmen, " +
-            "macht Spaß und ist aufschlussreich.</p>",
-          open: false,
-          height: 0
+            '<p>Als Mitglied der Familie Stihl bin ich im Beirat tätig, ' +
+            'der sich in erster Linie mit der Strategie beschäftigt. Beim STIHL Award war spannend, ' +
+            'in welcher Weise die Studierenden unsere strategischen Themenfelder mit verschiedenen ' +
+            'aktuellen Entwicklungen und auch ihren Wahrnehmungen und Herangehensweisen in Verbindung ' +
+            'gebracht haben. Zwischendurch die Blickrichtung zu ändern und neue Impulse aufzunehmen, ' +
+            'macht Spaß und ist aufschlussreich.</p>',
+          // open: false,
+          // height: 0
         },
-        accordion2: {
+        {
           header:
-            "Die eigene Innovationskraft erproben – Dr. Michael Prochaska",
+            'Die eigene Innovationskraft erproben – Dr. Michael Prochaska',
           body:
-            "<p>Was Nachwuchskräfte angeht, achten wir bei STIHL auf drei Dinge: die fachliche " +
-            "Qualifikation, die Extraaktivitäten neben Studium oder Ausbildung und die Chemie. " +
-            "Letztere ist uns besonders wichtig. Der STIHL CUTTING EDGE AWARD erfüllt alle drei Wünsche " +
-            "auf einmal: Die Teilnehmerinnen und Teilnehmer sind fachlich qualifiziert, engagiert und setzen " +
-            "sich intensiv mit unserem Arbeitsumfeld auseinander, um neue, innovative Ansätze zu finden.</p>",
-          open: false,
-          height: 0
+            '<p>Was Nachwuchskräfte angeht, achten wir bei STIHL auf drei Dinge: die fachliche ' +
+            'Qualifikation, die Extraaktivitäten neben Studium oder Ausbildung und die Chemie. ' +
+            'Letztere ist uns besonders wichtig. Der STIHL CUTTING EDGE AWARD erfüllt alle drei Wünsche ' +
+            'auf einmal: Die Teilnehmerinnen und Teilnehmer sind fachlich qualifiziert, engagiert und setzen ' +
+            'sich intensiv mit unserem Arbeitsumfeld auseinander, um neue, innovative Ansätze zu finden.</p>',
+          // open: false,
+          // height: 0
         },
-        accordion3: {
-          header: "STIHL in einem neuen Licht – Anke Kleinschmit",
+        {
+          header: 'STIHL in einem neuen Licht – Anke Kleinschmit',
           body:
-            "<p>In der Jury zu sitzen, war nicht ganz einfach. Auf den ersten Blick haben mich alle Ideen und vor " +
-            "allem ihre Bandbreite enorm begeistert. Es ging um innovative Vertriebs- und Geschäftsmodelle, digitale " +
-            "Angebote sowie Produktoptimierungen und gänzlich neue Produktideen. Letztere haben mich natürlich besonders " +
-            "interessiert.</p>",
-          open: false,
-          height: 0
+            '<p>In der Jury zu sitzen, war nicht ganz einfach. Auf den ersten Blick haben mich alle Ideen und vor ' +
+            'allem ihre Bandbreite enorm begeistert. Es ging um innovative Vertriebs- und Geschäftsmodelle, digitale ' +
+            'Angebote sowie Produktoptimierungen und gänzlich neue Produktideen. Letztere haben mich natürlich besonders ' +
+            'interessiert.</p>',
+          // open: false,
+          // height: 0
         },
-        accordion4: {
-          header: "Akademischer Spirit im Unternehmen – Dr. Tim Gegg",
+        {
+          header: 'Akademischer Spirit im Unternehmen – Dr. Tim Gegg',
           body:
-            "<p>Ich selbst bin vor mehr als 20 Jahren von der Uni Karlsruhe – mittlerweile ist es das Karlsruher Institut für " +
-            "Technologie (KIT) – als studentischer Mitarbeiter zu STIHL gekommen. Heute arbeite ich als Leiter der Digitalisierung " +
-            "eng mit Studierenden zusammen, da die Abteilung zu einem Großteil aus Studenten besteht.</p>",
-          open: false,
-          height: 0
+            '<p>Ich selbst bin vor mehr als 20 Jahren von der Uni Karlsruhe – mittlerweile ist es das Karlsruher Institut für ' +
+            'Technologie (KIT) – als studentischer Mitarbeiter zu STIHL gekommen. Heute arbeite ich als Leiter der Digitalisierung ' +
+            'eng mit Studierenden zusammen, da die Abteilung zu einem Großteil aus Studenten besteht.</p>',
+          // open: false,
+          // height: 0
         },
-      },
-    };
+      ],
+    }
   },
   methods: {
-    openCloseAccordion(item_ID) {
-      /* To Change open-Variable from false to true and contrariwise  */
-      this.items[item_ID].open = !this.items[item_ID].open;
-
-      /* To Dynamically Copy The Height it needs into the Element */
-      let accordionRef = 'accordion__body_' + item_ID;
-      this.items[item_ID].height = this.$refs[accordionRef][0].scrollHeight;
-
-      console.log("Add Event Listener 😍")
-
-      /* To Dynamically Change The Height When Resizing */
-      window.addEventListener("resize", () => {
-        let accordionRef = 'accordion__body_' + item_ID;
-        this.items[item_ID].height = this.$refs[accordionRef][0].scrollHeight;
-      });
+    itemClicked(data) {
+      console.log('This is Opened', data)
     },
   },
   // mounted: function () {
@@ -83,7 +82,7 @@ export default {
   //   console.log("beforeDestroy !!!")
   //   window.removeEventListener('resize', this.openCloseAccordion)
   // }
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -93,44 +92,5 @@ export default {
   overflow: hidden;
   border-radius: 4px;
   font-family: $acc-font;
-
-  &__header {
-    display: flex;
-    justify-content: space-between;
-    padding: 0.75rem 1rem;
-    width: 100%;
-    background: #1f8dd6;
-    border: none;
-    outline: none;
-    border-bottom: 1px solid #54a0ff;
-    color: #f3f3f3;
-    cursor: pointer;
-  }
-  &__header svg {
-    transition: all 0.5s ease-in-out;
-  }
-
-  &__header.active {
-    background-color: #1070b1;
-  }
-  &__header.active svg {
-    transform: rotate(180deg);
-    transition: all 0.5s ease-in-out;
-  }
-
-  &__body {
-    background-color: #f3f3f3;
-    max-height: 0;
-    overflow: hidden;
-    transition: all 0.5s ease-in-out;
-  }
-
-  &__content {
-    padding: 1rem 2.5rem 1rem 2.5rem;
-  }
-
-  p {
-    margin: 0;
-  }
 }
 </style>
---
+++ components/AccordionItems.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="accordion__item" :class="{ active: open }">
+    <button class="accordion__header" v-on:click="toggleAccordion">
+      <strong>{{ header }}</strong
+      ><font-awesome-icon :icon="['fas', 'angle-down']" />
+    </button>
+    <div
+      ref="accordionBodyRef"
+      :style="[open ? { 'max-height': `${height}px` } : { 'max-height': '0' }]"
+      class="accordion__body"
+    >
+      <div class="accordion__content" v-html="body"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AccordionItems',
+  data() {
+    return {
+      open: false,
+      height: 0,
+    }
+  },
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    header: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    toggleAccordion() {
+      // To Change open-Variable from false to true and contrariwise
+      this.open = !this.open
+
+      // To Dynamically Copy The Height it needs into the Element
+      this.height = this.$refs.accordionBodyRef.scrollHeight
+      this.$emit('clicked', { el: this.$el, open: this.open })
+
+      //   console.log('Add Event Listener 😍')
+      // To Dynamically Change The Height When Resizing
+      window.addEventListener('resize', () => {
+        this.height = this.$refs.accordionBodyRef.scrollHeight
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.accordion__header {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.75rem 1rem;
+  width: 100%;
+  background: #1f8dd6;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid #54a0ff;
+  color: #f3f3f3;
+  cursor: pointer;
+}
+
+.accordion__header svg {
+  transition: all 0.5s ease-in-out;
+}
+
+.accordion__header {
+  .active & {
+    background-color: #1070b1;
+  }
+}
+
+.accordion__header {
+  .active svg {
+    transform: rotate(180deg);
+    transition: all 0.5s ease-in-out;
+  }
+}
+
+.accordion__body {
+  background-color: #f3f3f3;
+  max-height: 0;
+  overflow: hidden;
+  transition: all 0.5s ease-in-out;
+}
+
+.accordion__content {
+  padding: 1rem 2.5rem 1rem 2.5rem;
+  ::v-deep {
+    p {
+      margin: 0;
+    }
+  }
+}
+</style>
