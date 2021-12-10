7c8b077ca48885c63053ab8d96f7bc2f841dc84c
shsal <s.saliba@s-v.de>
2021-12-10 17:15:35 +0100

    Done

---
+++ pages/Products.vue
@@ -0,0 +1,21 @@
+<template>
+  <Products :products="products" />
+</template>
+
+<script>
+import Products from '~/components/Products.vue'
+
+export default {
+  components: {
+    Products,
+  },
+  async asyncData({ $axios }) {
+    // const api_url = "https://fakestoreapi.com/products?limit=12"
+    const api_url = 'https://fakestoreapi.com/products'
+    const products = await $axios.$get(api_url)
+    return { products }
+  },
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,22 +6,20 @@
     <GridContent />
     <!-- <Overlay />
     <OverlayButton /> -->
-    <Products :products="products" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HeaderImage from "@/components/HeaderImage.vue";
-import HeaderText from "@/components/HeaderText.vue";
-import Accordion from "@/components/Accordion.vue";
-import GridContent from "@/components/GridContent.vue";
-import Overlay from "@/components/Overlay.vue";
-import OverlayButton from "@/components/OverlayButton.vue";
-import Products from '~/components/Products.vue';
+import HeaderImage from '@/components/HeaderImage.vue'
+import HeaderText from '@/components/HeaderText.vue'
+import Accordion from '@/components/Accordion.vue'
+import GridContent from '@/components/GridContent.vue'
+import Overlay from '@/components/Overlay.vue'
+import OverlayButton from '@/components/OverlayButton.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     HeaderImage,
     HeaderText,
@@ -29,13 +27,6 @@ export default {
     GridContent,
     Overlay,
     OverlayButton,
-    Products,
   },
-  async asyncData({$axios}) {
-    // const api_url = "https://fakestoreapi.com/products?limit=12"
-    const api_url = "https://fakestoreapi.com/products"
-    const products = await $axios.$get(api_url)
-    return { products }
-  },
-};
+}
 </script>
