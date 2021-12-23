25b9978ea1a78b449307466e187220086214fcd8
shsal <s.saliba@s-v.de>
2021-12-23 10:23:23 +0100

    Click Away Bug is now fixed (Modal Close When Clicking Inside The Modal)

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,18 @@
   <div>
     <Navbar />
     <Nuxt />
-    <Overlay2 v-show="showModal" @close-modal="showModal = false" />
+    <Overlay v-show="showModal" @close-modal="showModal = false" />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
+import Overlay from '../components/Overlay.vue'
 
 export default {
   components: {
     Navbar,
+    Overlay,
   },
   data() {
     return {
