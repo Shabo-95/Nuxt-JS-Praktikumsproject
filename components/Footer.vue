e567e62b8fec1e9e9ac426e70ed6d697fd7afb86
shsal <s.saliba@s-v.de>
2021-12-24 19:12:09 +0100

    The Footer is Looking Great and is Working Like a Charm

---
+++ components/Footer.vue
@@ -0,0 +1,94 @@
+<template>
+  <footer>
+    <div class="custom-container">
+      <ul class="custom-flex-row">
+        <li>
+          <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="nav-link" to="/Products">Produkte</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="nav-link" to="/Categories">Kategorien</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink class="nav-link" to="/About">About</NuxtLink>
+        </li>
+      </ul>
+      <ul class="custom-flex-row">
+        <li>
+          <a href="https://www.facebook.com/">
+            <font-awesome-icon :icon="['fab', 'facebook']" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/">
+            <font-awesome-icon :icon="['fab', 'instagram']" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/">
+            <font-awesome-icon :icon="['fab', 'twitter']" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/">
+            <font-awesome-icon :icon="['fab', 'youtube']" />
+          </a>
+        </li>
+      </ul>
+      <p>&copy; 2022 MyProject</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  color: #fff;
+  background-color: #004a99;
+  padding: 50px 20px;
+
+  .custom-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .custom-flex-row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin-bottom: 32px;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  li {
+    padding: 0 15px;
+    margin-bottom: 16px;
+  }
+
+  a {
+    font-size: 16px;
+    font-weight: 500;
+    color: #fff;
+  }
+
+  svg {
+    cursor: pointer;
+    font-size: 30px;
+  }
+
+  p {
+    text-align: center;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,20 @@
     <Navbar />
     <Nuxt />
     <Overlay v-show="showModal" @close-modal="showModal = false" />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar.vue'
 import Overlay from '../components/Overlay.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
   components: {
     Navbar,
     Overlay,
+    Footer,
   },
   data() {
     return {
