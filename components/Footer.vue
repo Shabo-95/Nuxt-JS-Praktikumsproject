6d3656396d4b883012a7aeea9e5f0b9f59015413
shsal <s.saliba@s-v.de>
2021-12-24 19:59:40 +0100

    Waves Added To The Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -18,26 +18,51 @@
       <ul class="custom-flex-row">
         <li>
           <a href="https://www.facebook.com/">
-            <font-awesome-icon :icon="['fab', 'facebook']" />
+            <font-awesome-icon
+              :icon="['fab', 'facebook']"
+              class="font-awesome"
+            />
           </a>
         </li>
         <li>
           <a href="https://www.instagram.com/">
-            <font-awesome-icon :icon="['fab', 'instagram']" />
+            <font-awesome-icon
+              :icon="['fab', 'instagram']"
+              class="font-awesome"
+            />
           </a>
         </li>
         <li>
           <a href="https://www.twitter.com/">
-            <font-awesome-icon :icon="['fab', 'twitter']" />
+            <font-awesome-icon
+              :icon="['fab', 'twitter']"
+              class="font-awesome"
+            />
           </a>
         </li>
         <li>
           <a href="https://www.youtube.com/">
-            <font-awesome-icon :icon="['fab', 'youtube']" />
+            <font-awesome-icon
+              :icon="['fab', 'youtube']"
+              class="font-awesome"
+            />
           </a>
         </li>
       </ul>
       <p>&copy; 2022 MyProject</p>
+    </div>
+    <div class="wave">
+      <svg
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1200 120"
+        preserveAspectRatio="none"
+      >
+        <path
+          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+          class="shape-fill"
+        ></path>
+      </svg>
     </div>
   </footer>
 </template>
@@ -51,11 +76,14 @@ footer {
   color: #fff;
   background-color: #004a99;
   padding: 50px 20px;
+  margin-top: 100px;
+  position: relative;
 
   .custom-container {
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-top: 64px;
   }
 
   .custom-flex-row {
@@ -82,13 +110,33 @@ footer {
     color: #fff;
   }
 
-  svg {
+  svg.font-awesome {
     cursor: pointer;
     font-size: 30px;
   }
 
   p {
     text-align: center;
+  }
+
+  .wave {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    overflow: hidden;
+    line-height: 0;
+  }
+
+  .wave svg {
+    position: relative;
+    display: block;
+    width: calc(178% + 1.3px);
+    height: 100px;
+  }
+
+  .wave .shape-fill {
+    fill: #ffffff;
   }
 }
 </style>
