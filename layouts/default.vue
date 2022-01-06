b00832204110d66f18fb45bd89c6d2c623972ff8
shsal <s.saliba@s-v.de>
2022-01-06 14:51:21 +0100

    Custom Navbar is now Added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Navbar />
+    <Navbar2 />
     <Nuxt />
     <Overlay v-show="showModal" @close-modal="showModal = false" />
     <Footer />
@@ -8,13 +8,13 @@
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
+import Navbar2 from '../components/Navbar2.vue'
 import Overlay from '../components/Overlay.vue'
 import Footer from '../components/Footer.vue'
 
 export default {
   components: {
-    Navbar,
+    Navbar2,
     Overlay,
     Footer,
   },
