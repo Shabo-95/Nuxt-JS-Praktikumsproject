e93a2cfac473857a4f8afd1a132ef78501c471f0
unknown <Shabo-95@hotmail.com>
2022-05-21 20:39:21 +0200

    Change Photo Grid Pictures

--- components/PhotoGrid.vue
+++ components/PhotoGrid.vue
@@ -2,67 +2,67 @@
   <div class="photo-grid">
     <div class="card card-tall card-wide">
       <img
-        src="https://images.unsplash.com/photo-1558981359-219d6364c9c8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+        src="https://source.unsplash.com/fPbLnMMd8BU/640x800"
         alt="image2"
       />
     </div>
     <div class="card card-wide">
       <img
-        src="https://images.unsplash.com/photo-1583585635793-0e1894c169bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=913&q=80"
+        src="https://source.unsplash.com/FT1ePvQ1HlE/640x800"
         alt="image2"
       />
     </div>
     <div class="card">
       <img
-        src="https://images.unsplash.com/photo-1583531172005-814191b8b6c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
+        src="https://source.unsplash.com/JC8ggPmYEMo/640x800"
         alt="image2"
       />
     </div>
     <div class="card">
       <img
-        src="https://images.unsplash.com/photo-1583445013765-46c20c4a6772?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+        src="https://source.unsplash.com/kKPWbHT5eMI/640x800"
         alt="image2"
       />
     </div>
     <div class="card card-wide">
       <img
-        src="https://images.unsplash.com/photo-1583426573939-97d09302d76a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=968&q=80"
+        src="https://source.unsplash.com/5mYVM_fDv/640x800"
         alt="image2"
       />
     </div>
     <div class="card">
       <img
-        src="https://images.unsplash.com/photo-1583532452513-a02186582ccd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+        src="https://source.unsplash.com/ShEvOOkm5SU/640x800"
         alt="image2"
       />
     </div>
     <div class="card card-wide">
       <img
-        src="https://images.unsplash.com/photo-1583562835057-a62d1beffbf3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80"
+        src="https://source.unsplash.com/__jmhIsDCK0/640x800"
         alt="image2"
       />
     </div>
     <div class="card card-tall card-wide">
       <img
-        src="https://images.unsplash.com/photo-1583483425010-c566431a7710?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1951&q=80"
+        src="https://source.unsplash.com/82auqXjSxZI/640x800"
         alt="image2"
       />
     </div>
     <div class="card card-tall">
       <img
-        src="https://images.unsplash.com/photo-1583468323330-9032ad490fed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80"
+        src="https://source.unsplash.com/Xo5CULzxaYE/640x800"
         alt="image2"
       />
     </div>
     <div class="card">
       <img
-        src="https://images.unsplash.com/photo-1583425423320-2386622cd2e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1980&q=80"
+        src="https://source.unsplash.com/T6PK5AaZ9ww/640x800"
         alt="image2"
       />
     </div>
     <div class="card">
       <img
-        src="https://images.unsplash.com/photo-1583518257225-f9a8081f6a84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+        src="https://source.unsplash.com/jKt5LwMYlxw/640x800"
         alt="image2"
       />
     </div>
